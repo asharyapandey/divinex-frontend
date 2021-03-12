3b8c03b8adfa21ac39a5f851a234079f35fd2ddf
asharyapandey <asharyap7@gmail.com>
2021-03-12 19:16:25 +0545

    made login a controlled component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./Auth.scss";
 import LoginImage from "./login.jpg";
 
 function Login() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 	return (
 		<div className="auth-container">
 			<div className="auth-container__content">
@@ -11,11 +14,21 @@ function Login() {
 					<form>
 						<div class="form-g">
 							<label for="username">Username:</label>
-							<input type="text" id="username" />
+							<input
+								type="text"
+								id="username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
 						</div>
 						<div class="form-g">
 							<label for="password">Password:</label>
-							<input type="password" id="password" />
+							<input
+								type="password"
+								id="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
 						</div>
 						<button type="submit" class="button">
 							Login
