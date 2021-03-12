9e6c1b3e84d30d375f9d9f8b72910953a95fe030
asharyapandey <asharyap7@gmail.com>
2021-03-12 19:19:08 +0545

    made register a controlled component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./Auth.scss";
 import RegisterImage from "./register.jpg";
 
 function Register() {
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
 	return (
 		<div className="auth-container">
 			<div className="auth-container__content">
@@ -11,21 +16,43 @@ function Register() {
 					<form>
 						<div class="form-g">
 							<label for="username">Username:</label>
-							<input type="text" id="username" />
+							<input
+								type="text"
+								id="username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
 						</div>
 						<div class="form-g">
 							<label for="email">Email:</label>
-							<input type="email" id="email" />
+							<input
+								type="email"
+								id="email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
 						</div>
 						<div class="form-g">
 							<label for="password">Password:</label>
-							<input type="password" id="password" />
+							<input
+								type="password"
+								id="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
 						</div>
 						<div class="form-g">
 							<label for="confirmPassword">
 								Confirm Password:
 							</label>
-							<input type="password" id="confirmPassword" />
+							<input
+								type="password"
+								id="confirmPassword"
+								value={confirmPassword}
+								onChange={(e) =>
+									setConfirmPassword(e.target.value)
+								}
+							/>
 						</div>
 						<button type="submit" class="button">
 							Register
