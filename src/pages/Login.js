5aedcdce30b1b9f5c2987e117ae529d57a94b127
asharyapandey <asharyap7@gmail.com>
2021-03-12 17:30:36 +0545

    added login component

---
+++ src/pages/Login.js
@@ -0,0 +1,28 @@
+function Login() {
+	return (
+		<div className="auth-container">
+			<div className="auth-container__content">
+				<div className="content">
+					<h1 className="heading">Divinex</h1>
+					<p>Login for Divine Experience</p>
+					<form>
+						<div class="form-g">
+							<label for="username">Username:</label>
+							<input type="text" id="username" />
+						</div>
+						<div class="form-g">
+							<label for="password">Password:</label>
+							<input type="password" id="password" />
+						</div>
+						<button type="submit" class="button">
+							Login
+						</button>
+					</form>
+				</div>
+			</div>
+			<div className="auth-container__image"></div>
+		</div>
+	);
+}
+
+export default Login;
