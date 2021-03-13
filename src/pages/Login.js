c46ecd0fc67fbeb92bf38bb7ef42d2804d3b48a9
asharyapandey <asharyap7@gmail.com>
2021-03-13 17:24:57 +0545

    added validaion in login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,13 +6,38 @@ import { Link } from "react-router-dom";
 function Login() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [usernameError, setUsernameError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const validate = () => {
+		if (username === "") {
+			setUsernameError("Username is Required");
+			return false;
+		} else {
+			setUsernameError("");
+		}
+		if (password === "") {
+			setPasswordError("Password is Required");
+			return false;
+		} else {
+			setPasswordError("");
+		}
+		return true;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (validate()) {
+			const data = { username, password };
+			console.log(data);
+		}
+	};
 	return (
 		<div className="auth-container">
 			<div className="auth-container__content">
 				<div className="content">
 					<h1 className="heading">DivineX</h1>
 					<p>Login for Divine Experience</p>
-					<form>
+					<form onSubmit={handleSubmit}>
 						<div className="form-g">
 							<label htmlFor="username">Username:</label>
 							<input
@@ -21,6 +46,7 @@ function Login() {
 								value={username}
 								onChange={(e) => setUsername(e.target.value)}
 							/>
+							<span className="error">{usernameError}</span>
 						</div>
 						<div className="form-g">
 							<label htmlFor="password">Password:</label>
@@ -30,6 +56,7 @@ function Login() {
 								value={password}
 								onChange={(e) => setPassword(e.target.value)}
 							/>
+							<span className="error">{passwordError}</span>
 						</div>
 						<button type="submit" className="button">
 							Login
