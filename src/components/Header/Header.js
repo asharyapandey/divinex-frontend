37a83b797cec86c1bac2b831065290d0e16e66bb
asharyapandey <asharyap7@gmail.com>
2021-03-21 13:31:02 +0545

    made navbar stick at the top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
 import Icon from "./Icon";
 import "./Header.scss";
 import Search from "./Search";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+	const [scrolled, setScrolled] = useState(false);
+
+	const handleScroll = () => {
+		const offset = window.pageYOffset;
+		if (offset > 0) setScrolled(true);
+		else setScrolled(false);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll");
+	}, []);
+
+	const classes = scrolled ? "container scrolled" : "container";
+
 	return (
-		<div className="container">
+		<div className={classes}>
 			<header className="header">
 				<div className="header__logo">DivineX</div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import CardList from "../components/PhotoCard/CardList";
 const Home = () => {
 	return (
 		<div>
-			<h1>This is the Home Page</h1>
 			<CardList />
 		</div>
 	);
