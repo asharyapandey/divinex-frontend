2c3f30271f5828e3662e7ad1d416092d80a40956
asharyapandey <asharyap7@gmail.com>
2021-03-21 13:52:55 +0545

    actual feed post can be seen now

--- src/components/PhotoCard/Card.js
+++ src/components/PhotoCard/Card.js
@@ -1,21 +1,27 @@
 import "./Card.scss";
 
-const Card = () => {
+const Card = ({ post }) => {
 	return (
 		<div className="card">
 			<div className="card__header">
 				<div className="card__header--profile-picture">
-					<img src="images/user.jpg" alt="" />
+					<img
+						src={
+							post.user.profilePicture ||
+							"images/profile_picture/male_img.png"
+						}
+						alt=""
+					/>
 				</div>
 				<div className="card__header--username">
-					<a href="#">asharyapandey</a>
+					<a href="#">{post.user.username}</a>
 				</div>
 				<div className="card__header--hamburger icon-comp">
 					<img src="images/more.png" alt="" />
 				</div>
 			</div>
 			<div className="card__image">
-				<img src="images/mock_img.jpg" alt="" />
+				<img src={post.image} alt="" />
 			</div>
 
 			<div className="card__footer">
